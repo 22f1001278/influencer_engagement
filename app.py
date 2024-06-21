eca2fafefaa3b96fecbcc1dae99027ba19c67abd
Abhinav <abhinavsonone@proton.me>
2024-06-21 23:59:05 +0530

    added flash & signup

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 ## imports
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash
+from datetime import timedelta
 from model import db, User
 
 
@@ -8,6 +9,10 @@ app = Flask(__name__)
 
 ## database configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ief_db.sqlite3'
+
+## set the secret key
+app.config['SECRET_KEY'] = '12345'
+
 ## initialize the app with model
 db.init_app(app)             ## connection between app and model
 app.app_context().push()      ## enables operation
@@ -29,18 +34,40 @@ def registration():
     
     if request.method == 'POST':
         email = request.form.get('email') 
-        password = request.form.get('password')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
 
-        user1 = User(email = email, password=password)
+        
+        if not email:
+            flash('Please check your email!','danger')
+            return redirect(url_for('registration'))
+        elif not password1:
+            flash('Please enter your password!','danger')
+            return redirect(url_for('registration'))
+        elif password1 != password2:
+            flash('Please check your password!','danger')
+            return redirect(url_for('registration'))
+        elif User.query.filter_by(email=email).first():
+            flash('User already exists','danger')
+            return redirect(url_for('registration'))
+
+        user1 = User(email = email, password=password1)
         db.session.add(user1)
         db.session.commit()
-        return redirect(url_for('login'))
+
+        flash("Registration Successful. Please Login to continue!",'success')
+        return redirect(url_for('registration'))  ## returning the same page temporarily 
     
 
-@app.route("/login")
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    current_user = 'user1'
-    return render_template('login.html', nav_type = user_type[current_user])
+    if request.method == 'POST':
+        current_user = 'user1'
+        return render_template('login.html', nav_type = user_type[current_user])
+    elif request.method == 'GET':
+        current_user = 'user1'
+        return render_template('login.html', nav_type = user_type[current_user])
+
 
 
 

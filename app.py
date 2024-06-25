3efcbfe490f83f0ec680937c25e1beacae9a9208
Abhinav <abhinavsonone@proton.me>
2024-06-26 00:42:31 +0530

    authentication added

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, url_for, request, redirect, flash, session
 from datetime import timedelta
 from model import db, User
-from helper import getUserInfo
+from helper import getUserInfo, influencerNeeded
 
 
 ## object of flask
@@ -33,7 +33,7 @@ def registration():
         email = request.form.get('email') 
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-
+        role = request.form.get('role')
         
         if not email:
             flash('Please check your email!','danger')
@@ -47,23 +47,23 @@ def registration():
         elif User.query.filter_by(email=email).first():
             flash('User already exists','danger')
             return redirect(url_for('registration'))
+        elif not role:
+            flash('Role is required', 'danger')
+            return redirect(url_for('registration'))
+        elif User.query.filter_by(email=email).first():
+            flash('User already exists', 'danger')
+            return redirect(url_for('registration'))
+        
+        if role == 'influencer':
+            user1 = User(email=email, password=password1, isInfluencer=1, isSponsor=0)
+        elif role == 'sponsor':
+            user1 = User(email=email, password=password1, isInfluencer=0, isSponsor=1)
 
-        user1 = User(email = email, password=password1)
         db.session.add(user1)
         db.session.commit()
-
-        nav_type = ''
-        if user1:
-            if user1.isAdmin:
-                nav_type = 'admin'
-            elif user1.isInfluencer:
-                nav_type = 'influencer'
-            elif user1.isSponsor:
-                nav_type = 'sponsor'
-
         flash("Registration Successful. Please Login to continue!",'success')
         
-        return render_template('login.html', nav_type = nav_type)
+        return render_template('registration.html', nav_type = user1)
     
     if request.method == 'GET':
         current_user = getUserInfo()
@@ -90,28 +90,35 @@ def login():
         elif current_user:
             session['id'] = current_user.id
 
-            nav_type = ''
-            if current_user:
-                if current_user.isAdmin:
-                    role = 'admin'
-                elif current_user.isInfluencer:
-                    role = 'influencer'
-                elif current_user.isSponsor:
-                    role = 'sponsor'
             flash("Login Successful! ",'success')
-            return render_template('index.html', nav_type = {'role':role, 'email':current_user.email})
+            return render_template('index.html', nav_type = current_user)
     
     ## handling the GET method
 
     current_user = getUserInfo() ## will return email and role
     return render_template('login.html', nav_type=current_user )
 
-@app.route('/logout', methods=['GET', 'POST'])
+@app.route('/logout', methods=['POST','GET'])
 def logout():
     session.pop('id',None)
     flash('Logout Successful','success')
-    return redirect(url_for('index'))
+    return redirect(url_for('login'))
 
+
+@app.route('/profile',methods =['GET','POST'])
+@influencerNeeded()
+def profile():
+    user = getUserInfo()
+    ## check the authorization
+    if 'id' not in session:
+        return redirect(url_for('login'))
+    
+    id = session['id']
+    user = User.query.filter_by(id=id).first()
+    if user is None or not user.isInfluencer:
+        return redirect(url_for('login'))
+    
+    return render_template('profile.html', nav_type=user)
 
 if __name__ == "__main__": 
     app.run(host='0.0.0.0',debug =True)
--- helper.py
+++ helper.py
@@ -1,4 +1,4 @@
-from flask import session, redirect, url_for
+from flask import session, redirect, url_for, flash
 from model import User
 from functools import wraps
 
@@ -7,26 +7,46 @@ def getUserInfo():
     if 'id' in session:
         current_user = User.query.filter_by(id = session['id']).first()
         email = current_user.email
-        if current_user:
-            if current_user.isAdmin:
-                role = 'admin'
-            elif current_user.isInfluencer:
-                role = 'influencer'
-            elif current_user.isSponsor:
-                role = 'sponsor'
-        return {'email':email, 'role':role}
+        isAdmin = current_user.isAdmin
+        isInfluencer = current_user.isInfluencer
+        isSponsor = current_user.isSponsor
+        return {'email':email, 'isAdmin':isAdmin, 'isInfluencer':isInfluencer, 'isSponsor':isSponsor}
     
     else:
-        return {'role':''}
+        return None
     
 
+def influencerNeeded():
+    def wrapper(f):
+        @wraps(f)
+        def decorator(*args, **kwargs):
+            user = getUserInfo()
+            if user is None or 'isInfluencer' not in user or user['isInfluencer'] != 1:
+                flash('Influencer only','danger')
+                return redirect(url_for('logout'))
+            return f(*args, **kwargs) 
+        return decorator
+    return wrapper
 
-# def user_needed(f):
-#     @wraps(f) ## changes the name of the function returned to the name of the function passed
-#     def decorated_function(*args, **kwargs):
-#         user = getUserInfo()
-#         if user['role'] not in ['isInfluencer', 'isSponsor', 'isAdmin']:
-#             flash('user only','danger')
-#             return redirect(url_for('logout'))
-#         return f(*args, **kwargs)
-#     return decorated_function
+def sponsorNeeded():
+    def wrapper(f):
+        @wraps(f)
+        def decorator(*args, **kwargs):
+            user = getUserInfo()
+            if not user['isSponsor']:
+                flash('Sponsor only','danger')
+                return redirect(url_for('logout'))
+        return decorator
+    return wrapper
+
+
+def adminNeeded():
+    def wrapper(f):
+        @wraps(f)
+        def decorator(*args, **kwargs):
+            user = getUserInfo()
+            if user['isAdmin'] != 1:
+                flash('Admin only','danger')
+                return redirect(url_for('logout'))
+        return decorator
+    return wrapper
